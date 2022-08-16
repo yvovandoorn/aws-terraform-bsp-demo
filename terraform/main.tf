5a80cc18cf69be28cf59d94ed016a1eeb0722641
yvovandoorn <yvo.vandoorn@gmail.com>
2022-08-16 16:26:06 +0200

    get rid of required plugins all together

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-
-  required_version = "~> 0.12"
-
   backend "s3" {
     bucket = "mondoo-demo-s3-terraform-backend"
     key    = "terraform.tfstate"
