617531e89963795f8c20daf950f927675d577d01
yvovandoorn <yvo.vandoorn@gmail.com>
2022-08-16 15:39:56 +0200

    making some changes so github actions can deploy.

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,12 @@ terraform {
   }
 
   required_version = ">= 0.14.9"
+
+  backend "s3" {
+    bucket = "mondoo-demo-s3-terraform-backend"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+   }
 }
 
 # Default set to AWS eu-central-1
@@ -17,7 +23,27 @@ provider "aws" {
 
 # KMS key to be used to encrypt data on buckets
 
-resource "aws_s3_bucket" "files" {
-  bucket = "yvodemo-awss3-mondoodemo-files-bucket"
-  acl    = "private"
+resource "aws_s3_bucket" "website" {
+  bucket = "mondoo-static-website-bucket"
+  acl    = "public-read"
+  policy  = <<EOF
+{
+     "id" : "MakePublic",
+   "version" : "2012-10-17",
+   "statement" : [
+      {
+         "action" : [
+             "s3:GetObject"
+          ],
+         "effect" : "Allow",
+         "resource" : "arn:aws:s3:::[BUCKET_NAME_HERE]/*",
+         "principal" : "*"
+      }
+    ]
+  }
+EOF
+
+  website {
+      index_document = "index.html"
+   }
 }
