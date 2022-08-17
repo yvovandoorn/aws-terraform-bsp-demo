ade05402d658d5b0be2f19ddd8182cb7ae6d0202
yvovandoorn <yvo.vandoorn@gmail.com>
2022-08-17 11:47:39 +0200

    set backend information at the CI level instead of in the tf config

--- main.tf
+++ main.tf
@@ -12,9 +12,7 @@ terraform {
   required_version = ">= 0.14.9"
 
   backend "s3" {
-    bucket = "mondoo-demo-s3-terraform-backend"
     key    = "terraform.tfstate"
-    region = "${var.aws-region}"
   }
 }
 
