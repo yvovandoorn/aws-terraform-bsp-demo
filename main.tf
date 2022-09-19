bf92d5ff5aa3f7bf927b5c46c5f76c03763b5fd3
yvovandoorn <yvo.vandoorn@gmail.com>
2022-09-19 12:48:24 +0200

    going to a 100

--- main.tf
+++ main.tf
@@ -38,3 +38,119 @@ resource "aws_s3_bucket_acl" "files" {
 }
 
 ####
+resource "aws_s3_bucket_versioning" "files" {
+  bucket = aws_s3_bucket.files.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_logging" "files" {
+  bucket = aws_s3_bucket.files.id
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log-${aws_s3_bucket.files.id}/"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "files" {
+bucket = aws_s3_bucket.files.id
+rule {
+  apply_server_side_encryption_by_default {
+    kms_master_key_id = aws_kms_key.mykey.arn
+    sse_algorithm = "aws:kms"
+  }
+}
+}
+
+resource "aws_s3_bucket_public_access_block" "files" {
+  bucket = aws_s3_bucket.files.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Setup the logging bucket
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "${var.files-bucket-name}-logbucket"
+}
+
+resource "aws_s3_bucket_acl" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+  acl = "private"
+}
+
+resource "aws_s3_bucket_versioning" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket" {
+bucket = aws_s3_bucket.log_bucket.id
+rule {
+  apply_server_side_encryption_by_default {
+    kms_master_key_id = aws_kms_key.mykey.arn
+    sse_algorithm = "aws:kms"
+  }
+}
+}
+
+resource "aws_s3_bucket_logging" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+  target_bucket = aws_s3_bucket.log_bucket2.id
+  target_prefix = "log-${aws_s3_bucket.log_bucket.id}/"
+}
+
+resource "aws_s3_bucket_public_access_block" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Setup the second logging bucket
+resource "aws_s3_bucket" "log_bucket2" {
+  bucket = "${var.files-bucket-name}-logbucket2"
+}
+
+resource "aws_s3_bucket_acl" "log_bucket2" {
+  bucket = aws_s3_bucket.log_bucket2.id
+  acl = "private"
+}
+
+resource "aws_s3_bucket_versioning" "log_bucket2" {
+  bucket = aws_s3_bucket.log_bucket2.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket2" {
+bucket = aws_s3_bucket.log_bucket2.id
+rule {
+  apply_server_side_encryption_by_default {
+    kms_master_key_id = aws_kms_key.mykey.arn
+    sse_algorithm = "aws:kms"
+  }
+}
+}
+
+resource "aws_s3_bucket_logging" "log_bucket2" {
+  bucket = aws_s3_bucket.log_bucket2.id
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log-${aws_s3_bucket.log_bucket2.id}/"
+}
+
+resource "aws_s3_bucket_public_access_block" "log_bucket2" {
+  bucket = aws_s3_bucket.log_bucket2.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
